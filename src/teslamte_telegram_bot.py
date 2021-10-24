730d2016d041df3ad2d1741229b99ec471eb8388
Nani Shin <nani.shin@gmail.com>
2021-10-24 23:24:12 +0900

    Add button control

--- src/teslamte_telegram_bot.py
+++ src/teslamte_telegram_bot.py
@@ -6,6 +6,9 @@ import paho.mqtt.client as mqtt
 from telegram.bot import Bot
 from telegram.parsemode import ParseMode
 
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+
 # initializing the bot with API_KEY and CHAT_ID
 if os.getenv('TELEGRAM_BOT_API_KEY') == None:
     print("Error: Please set the environment variable TELEGRAM_BOT_API_KEY and try again.")
@@ -16,6 +19,12 @@ if os.getenv('TELEGRAM_BOT_CHAT_ID') == None:
     print("Error: Please set the environment variable TELEGRAM_BOT_CHAT_ID and try again.")
     exit(1)
 chat_id = os.getenv('TELEGRAM_BOT_CHAT_ID')
+
+charger_phases = None
+update_version = None
+scheduled_charging_start_time = None
+geofence = None
+shift_state = None
 
 # based on example from https://pypi.org/project/paho-mqtt/
 # The callback for when the client receives a CONNACK response from the server.
@@ -39,107 +48,336 @@ def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload.decode()))
     title = msg.topic
     value = str(msg.payload.decode())
+
+    global display_name
+    global state
+    global since
+    global healthy
+    global version
+    global update_available
+    global update_version
+    global model
+    global trim_badging
+    global wheel_type
+    global exterior_color
+    global spoiler_type
+    global geofence
+    global latitude
+    global longitude
+    global shift_state
+    global power
+    global speed
+    global heading
+    global elevation
+    global locked
+    global sentry_mode
+    global windows_open
+    global doors_open
+    global trunk_open
+    global frunk_open
+    global is_user_present
+    global is_climate_on
+    global inside_temp
+    global outside_temp
+    global is_preconditioning
+    global odometer
+    global est_battery_range_km
+    global rated_battery_range_km
+    global ideal_battery_range_km
+    global battery_level
+    global usable_battery_level
+    global plugged_in
+    global charge_energy_added
+    global charge_limit_soc
+    global charge_port_door_open
+    global charger_actual_current
+    global charger_phases
+    global charger_power
+    global charger_voltage
+    global scheduled_charging_start_time
+    global time_to_full_charge
+
     # Check Teslamate MQTT Topics based on https://docs.teslamate.org/docs/integrations/mqtt
     if msg.topic == "teslamate/cars/1/display_name":
-        title = "<b>Display Name</b> : "
+        display_name = value
     elif msg.topic == "teslamate/cars/1/state":
-        title = "<b>State</b> : "
+        state = value
     elif msg.topic == "teslamate/cars/1/since":
-        title = "<b>Since</b> : "
+        since = value
     elif msg.topic == "teslamate/cars/1/healthy":
-        title = "<b>Healthy</b> : "
+        healthy = value
     elif msg.topic == "teslamate/cars/1/version":
-        title = "<b>Version</b> : "
+        version = value
     elif msg.topic == "teslamate/cars/1/update_available":
-        title = "<b>State</b> : "
+        update_available = value
     elif msg.topic == "teslamate/cars/1/update_version":
-        title = "<b>Update Version</b> : "
+        update_version = value
     elif msg.topic == "teslamate/cars/1/model":
-        title = "<b>Model</b> : "
+        model = value
     elif msg.topic == "teslamate/cars/1/trim_badging":
-        title = "<b>Trim Badging</b> : "
+        trim_badging = value
     elif msg.topic == "teslamate/cars/1/exterior_color":
-        title = "<b>Exterior Color</b> : "
+        exterior_color = value
     elif msg.topic == "teslamate/cars/1/wheel_type":
-        title = "<b>Wheel Type</b> : "
+        wheel_type = value
     elif msg.topic == "teslamate/cars/1/spoiler_type":
-        title = "<b>Spoiler Type</b> : "
+        spoiler_type = value
     elif msg.topic == "teslamate/cars/1/geofence":
-        title = "<b>Geofence</b> : "
+        geofence = value
     elif msg.topic == "teslamate/cars/1/latitude":
-        title = "<b>latitude</b> : "
+        latitude = value
     elif msg.topic == "teslamate/cars/1/longitude":
-        title = "<b>Longitude</b> : "
+        longitude = value
     elif msg.topic == "teslamate/cars/1/shift_state":
-        title = "<b>Shift State</b> : "
+        shift_state = value
     elif msg.topic == "teslamate/cars/1/power":
-        title = "<b>Power</b> : "
+        power = value
     elif msg.topic == "teslamate/cars/1/speed":
-        title = "<b>Speed</b> : "
+        speed = value
     elif msg.topic == "teslamate/cars/1/heading":
-        title = "<b>Heading</b> : "
+        heading = value
     elif msg.topic == "teslamate/cars/1/elevation":
-        title = "<b>Elevation</b> : "
+        elevation = value
     elif msg.topic == "teslamate/cars/1/locked":
-        title = "<b>Locked</b> : "
+        locked = value
     elif msg.topic == "teslamate/cars/1/sentry_mode":
-        title = "<b>Sentry Mode</b> : "
+        sentry_mode = value
     elif msg.topic == "teslamate/cars/1/windows_open":
-        title = "<b>Windows Open</b> : "
+        windows_open = value
     elif msg.topic == "teslamate/cars/1/doors_open":
-        title = "<b>Doors Open</b> : "
+        doors_open = value
     elif msg.topic == "teslamate/cars/1/trunk_open":
-        title = "<b>Trunk Open</b> : "
+        trunk_open = value
     elif msg.topic == "teslamate/cars/1/frunk_open":
-        title = "<b>Frunk Open</b> : "
+        frunk_open = value
     elif msg.topic == "teslamate/cars/1/is_user_present":
-        title = "<b>Is User Present</b> : "
+        is_user_present = value
     elif msg.topic == "teslamate/cars/1/is_climate_on":
-        title = "<b>Is Climate On</b> : "
+        is_climate_on = value
     elif msg.topic == "teslamate/cars/1/inside_temp":
-        title = "<b>Inside Temp</b> : "
+        inside_temp = value
     elif msg.topic == "teslamate/cars/1/outside_temp":
-        title = "<b>Outside Temp</b> : "
+        outside_temp = value
     elif msg.topic == "teslamate/cars/1/is_preconditioning":
-        title = "<b>Is Preconditioning</b> : "
+        is_preconditioning = value
     elif msg.topic == "teslamate/cars/1/odometer":
-        title = "<b>Odometer</b> : "
+        odometer = value
     elif msg.topic == "teslamate/cars/1/est_battery_range_km":
-        title = "<b>Est Battery Range Km</b> : "
+        est_battery_range_km = value
     elif msg.topic == "teslamate/cars/1/rated_battery_range_km":
-        title = "<b>Rated Battery Range Km</b> : "
+        rated_battery_range_km = value
     elif msg.topic == "teslamate/cars/1/ideal_battery_range_km":
-        title = "<b>Ideal Battery Range Km</b> : "
+        ideal_battery_range_km = value
     elif msg.topic == "teslamate/cars/1/battery_level":
-        title = "<b>Battery Level</b> : "
+        battery_level = value
     elif msg.topic == "teslamate/cars/1/usable_battery_level":
-        title = "<b>Usable Battery Level</b> : "
+        usable_battery_level = value
     elif msg.topic == "teslamate/cars/1/plugged_in":
-        title = "<b>Plugged In</b> : "
+        plugged_in = value
     elif msg.topic == "teslamate/cars/1/charge_energy_added":
-        title = "<b>Charge Energy Added</b> : "
+        charge_energy_added = value
     elif msg.topic == "teslamate/cars/1/charge_limit_soc":
-        title = "<b>Charge Limit</b> : "
+        charge_limit_soc = value
     elif msg.topic == "teslamate/cars/1/charge_port_door_open":
-        title = "<b>Charge Port Door Open</b> : "
+        charge_port_door_open = value
     elif msg.topic == "teslamate/cars/1/charger_actual_current":
-        title = "<b>Charger Actual Current</b> : "
+        charger_actual_current = value
     elif msg.topic == "teslamate/cars/1/charger_phases":
-        title = "<b>Charger Phases</b> : "
+        charger_phases = value
     elif msg.topic == "teslamate/cars/1/charger_power":
-        title = "<b>Charger Power</b> : "
+        charger_power = value
     elif msg.topic == "teslamate/cars/1/charger_voltage":
-        title = "<b>Charger Voltage</b> : "
+        charger_voltage = value
     elif msg.topic == "teslamate/cars/1/scheduled_charging_start_time":
-        title = "<b>Scheduled Charging Start Time</b> : "
+        scheduled_charging_start_time = value
     elif msg.topic == "teslamate/cars/1/time_to_full_charge":
-        title = "<b>Time To Full Charge</b> : "
+        time_to_full_charge = value
+    else:
+        bot.send_message(
+            chat_id,
+            text = title + " : " + value,
+            parse_mode=ParseMode.HTML,
+        )
 
-    bot.send_message(
-        chat_id,
-        text = title + value,
-        parse_mode=ParseMode.HTML,
-    )
+def start(update: Update, context: CallbackContext) -> None:
+    """Sends a message with four inline buttons attached."""
+    keyboard = [
+        [
+            InlineKeyboardButton("Version", callback_data='version'),
+            InlineKeyboardButton("Odometer", callback_data='odometer'),
+        ],
+        [
+            InlineKeyboardButton("Battery", callback_data='battery'),
+            InlineKeyboardButton("What Else", callback_data='what_else'),
+        ],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+
+def button(update: Update, context: CallbackContext) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    query.answer()
+
+    global display_name
+    global state
+    global since
+    global healthy
+    global version
+    global update_available
+    global update_version
+    global model
+    global trim_badging
+    global wheel_type
+    global exterior_color
+    global spoiler_type
+    global geofence
+    global latitude
+    global longitude
+    global shift_state
+    global power
+    global speed
+    global heading
+    global elevation
+    global locked
+    global sentry_mode
+    global windows_open
+    global doors_open
+    global trunk_open
+    global frunk_open
+    global is_user_present
+    global is_climate_on
+    global inside_temp
+    global outside_temp
+    global is_preconditioning
+    global odometer
+    global est_battery_range_km
+    global rated_battery_range_km
+    global ideal_battery_range_km
+    global battery_level
+    global usable_battery_level
+    global plugged_in
+    global charge_energy_added
+    global charge_limit_soc
+    global charge_port_door_open
+    global charger_actual_current
+    global charger_phases
+    global charger_power
+    global charger_voltage
+    global scheduled_charging_start_time
+    global time_to_full_charge
+
+    if query.data == "version":
+        text = f"Version : {version}"
+    elif query.data == "odometer":
+        text = f"Odometer : {odometer}"
+    elif query.data == "battery":
+        if est_battery_range_km is not None:
+            battery_value = f"Est Battery Range Km : {est_battery_range_km}\n"
+        if rated_battery_range_km is not None:
+            battery_value = f"{battery_value}Rated Battery Range Km : {rated_battery_range_km}\n"
+        if ideal_battery_range_km is not None:
+            battery_value = f"{battery_value}Ideal Battery Range Km : {ideal_battery_range_km}\n"
+        if battery_level is not None:
+            battery_value = f"{battery_value}Battery Level : {battery_level}\n"
+        if usable_battery_level is not None:
+            battery_value = f"{battery_value}Usable Battery Level : {usable_battery_level}\n"
+        if plugged_in is not None:
+            battery_value = f"{battery_value}Plugged In : {plugged_in}\n"
+        if charge_energy_added is not None:
+            battery_value = f"{battery_value}Charge Energy Added : {charge_energy_added}\n"
+        if charge_limit_soc is not None:
+            battery_value = f"{battery_value}Charge Limit SOC : {charge_limit_soc}\n"
+        if charge_port_door_open is not None:
+            battery_value = f"{battery_value}Charge Port Door Open : {charge_port_door_open}\n"
+        if charger_actual_current is not None:
+            battery_value = f"{battery_value}Charger Actual Current : {charger_actual_current}\n"
+        if charger_phases is not None:
+            battery_value = f"{battery_value}Charger Phases : {charger_phases}\n"
+        if charger_power is not None:
+            battery_value = f"{battery_value}Charger Power : {charger_power}\n"
+        if charger_voltage is not None:
+            battery_value = f"{battery_value}Charger Voltage : {charger_voltage}\n"
+        if scheduled_charging_start_time is not None:
+            battery_value = f"{battery_value}Scheduled Charging Start Time : {scheduled_charging_start_time}\n"
+        if time_to_full_charge is not None:
+            battery_value = f"{battery_value}Time To Full Charge : {time_to_full_charge}\n"
+
+        text = battery_value
+    elif query.data == "what_else":
+        if display_name is not None:
+            what_else_value = f"Display Name : {display_name}\n"
+        if state is not None:
+            what_else_value = f"{what_else_value}State : {state}\n"
+        if since is not None:
+            what_else_value = f"{what_else_value}Since : {since}\n" 
+        if healthy is not None:
+            what_else_value = f"{what_else_value}Healthy : {healthy}\n"
+        if update_available is not None:
+            what_else_value = f"{what_else_value}Update Available : {update_available}\n"
+        if update_version is not None:
+            what_else_value = f"{what_else_value}Update Version : {update_version}\n"
+        if model is not None:
+            what_else_value = f"{what_else_value}Model : {model}\n"
+        if trim_badging is not None:
+            what_else_value = f"{what_else_value}Trim Badging : {trim_badging}\n"
+        if wheel_type is not None:
+            what_else_value = f"{what_else_value}Wheel Type : {wheel_type}\n"
+        if exterior_color is not None:
+            what_else_value = f"{what_else_value}Exterior Color : {exterior_color}\n"
+        if spoiler_type is not None:
+            what_else_value = f"{what_else_value}Spoiler Type : {spoiler_type}\n"
+        if geofence is not None:
+            what_else_value = f"{what_else_value}Geofence : {geofence}\n"
+        if latitude is not None:
+            what_else_value = f"{what_else_value}Latitude : {latitude}\n"
+        if longitude is not None:
+            what_else_value = f"{what_else_value}Longitude : {longitude}\n"
+        if shift_state is not None:
+            what_else_value = f"{what_else_value}Shift State : {shift_state}\n"
+        if power is not None:
+            what_else_value = f"{what_else_value}Power : {power}\n"
+        if speed is not None:
+            what_else_value = f"{what_else_value}Speed : {speed}\n"
+        if heading is not None:
+            what_else_value = f"{what_else_value}Heading : {heading}\n"
+        if elevation is not None:
+            what_else_value = f"{what_else_value}Elevation : {elevation}\n"
+        if locked is not None:
+            what_else_value = f"{what_else_value}Locked : {locked}\n"
+        if sentry_mode is not None:
+            what_else_value = f"{what_else_value}Sentry Mode : {sentry_mode}\n"
+        if windows_open is not None:
+            what_else_value = f"{what_else_value}Windows Open : {windows_open}\n"
+        if doors_open is not None:
+            what_else_value = f"{what_else_value}Doors Open : {doors_open}\n"
+        if trunk_open is not None:
+            what_else_value = f"{what_else_value}Trunk Open : {trunk_open}\n"
+        if frunk_open is not None:
+            what_else_value = f"{what_else_value}Frunk Open : {frunk_open}\n"
+        if is_user_present is not None:
+            what_else_value = f"{what_else_value}Is User Present : {is_user_present}\n"
+        if is_climate_on is not None:
+            what_else_value = f"{what_else_value}Is Climate On : {is_climate_on}\n"
+        if inside_temp is not None:
+            what_else_value = f"{what_else_value}Inside Temp : {inside_temp}\n"
+        if outside_temp is not None:
+            what_else_value = f"{what_else_value}Outside Temp : {outside_temp}\n"
+        if is_preconditioning is not None:
+            what_else_value = f"{what_else_value}Is Preconditioning : {is_preconditioning}\n"
+
+        text = what_else_value
+
+    query.edit_message_text(text)
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Displays info on how to use the bot."""
+    update.message.reply_text("Use /start to check YOUR_BOT_NAME.")
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -163,6 +401,11 @@ client.connect(os.getenv('MQTT_BROKER_HOST', '127.0.0.1'),
 # manual interface.
 #client.loop_forever()
 
+updater = Updater(os.getenv('TELEGRAM_BOT_API_KEY'))
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CallbackQueryHandler(button))
+updater.dispatcher.add_handler(CommandHandler('help', help_command))
+
 client.loop_start()  # start the loop
 try:
     bot.send_message(
@@ -172,6 +415,9 @@ try:
         )
 
     while True:
+
+        # Start the Bot
+        updater.start_polling()
 
         time.sleep(1)
 
